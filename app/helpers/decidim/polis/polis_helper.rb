e6b31be839c355ea8d7b8bcecaae1f83ff21f554
takahashim <takahashimm@gmail.com>
2023-05-08 19:05:40 +0900

    Fix avatar_url

--- app/helpers/decidim/polis/polis_helper.rb
+++ app/helpers/decidim/polis/polis_helper.rb
@@ -4,8 +4,8 @@ module Decidim
   module Polis
     module PolisHelper
       def avatar_url
-        avatar_path = if current_user.avatar.attached?
-                        current_user.avatar.default_url
+        avatar_path = if current_user.presenter && current_user.presenter.respond_to?(:avatar_url)
+                        current_user.presenter.avatar_url
                       else
                         ActionController::Base.helpers.asset_pack_path("media/images/default-avatar.svg")
                       end
