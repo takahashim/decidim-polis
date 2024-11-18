57742dc52570172d31eb9a6aa5e08ebfb821bec1
takahashim <takahashimm@gmail.com>
2024-11-18 20:16:55 +0900

    update 0.28.3

--- lib/decidim/polis/version.rb
+++ lib/decidim/polis/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module Polis
     def self.version
-      "0.28.0"
+      "0.28.3"
     end
 
     def self.decidim_version
-      "0.28.0"
+      "0.28.3"
     end
   end
 end
